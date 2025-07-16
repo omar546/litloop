ebc5e685a92cadbcf92186fb26006c860e4e0c56
omar546 <omer.mkawy.om@gmail.com>
2025-07-16 13:23:26 +0300

    featured_item.dart

--- lib/features/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/data/presentation/views/widgets/home_view_body.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import '../../../../../splash/presentation/views/widgets/custom_app_bar.dart';
+import '../../../../../splash/presentation/views/widgets/featured_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [CustomAppBar()]);
+    return const Column(children: [CustomAppBar(),FeaturedItem()]);
   }
 }
---
+++ lib/features/splash/presentation/views/widgets/featured_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class FeaturedItem extends StatelessWidget {
+  const FeaturedItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height * 0.3,
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            //todo: remove static image
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                'https://bukovero.com/wp-content/uploads/2016/07/Harry_Potter_and_the_Cursed_Child_Special_Rehearsal_Edition_Book_Cover.jpg',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
