05961f675617ef6df5b50b88698507f63e40ecf6
omar546 <omer.mkawy.om@gmail.com>
2025-07-16 13:06:53 +0300

    custom_app_bar.dart

---
+++ lib/features/home/data/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../../../../../splash/presentation/views/widgets/custom_app_bar.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(children: [CustomAppBar()]);
+  }
+}
---
+++ lib/features/splash/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0,vertical: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(AssetsData.logo, height: 40),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(FontAwesomeIcons.magnifyingGlass),
+          ),
+        ],
+      ),
+    );
+  }
+}
