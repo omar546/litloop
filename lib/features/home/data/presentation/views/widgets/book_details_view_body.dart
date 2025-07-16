b28a0a86a211a86637ec3fbffd449cca8b214c89
omar546 <omer.mkawy.om@gmail.com>
2025-07-16 22:32:51 +0300

    add custom scroll

--- lib/features/home/data/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/data/presentation/views/widgets/book_details_view_body.dart
@@ -13,46 +13,53 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        spacing: 10,
-        children: [
-          CustomAppBarDetailsView(),
-          SizedBox(
-            height: MediaQuery.sizeOf(context).height * 0.3,
-            child: CustomBookItem(showPlayButton: false),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Text(
-              'Harry Potter and the Goblet of Fire',
-              style: Styles.textStyle30,
-              maxLines: 2,
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.ellipsis,
+      child: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              spacing: 10,
+              children: [
+                CustomAppBarDetailsView(),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height * 0.3,
+                  child: CustomBookItem(showPlayButton: false),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Text(
+                    'Harry Potter and the Goblet of Fire',
+                    style: Styles.textStyle30,
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Text(
+                  "J.K.Rowling",
+                  style: Styles.textStyle18.copyWith(
+                    color: Colors.grey,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const Rating(mainAlignment: MainAxisAlignment.center),
+                BooksAction(),
+                const Spacer(),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Text('You may also like', style: Styles.textStyle18),
+                  ),
+                ),
+                YouMayLikeList(),
+              ],
             ),
           ),
-          Text(
-            "J.K.Rowling",
-            style: Styles.textStyle18.copyWith(
-              color: Colors.grey,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.w500,
-            ),
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-          ),
-          const Rating(mainAlignment: MainAxisAlignment.center),
-          BooksAction(),
-          SizedBox(height: 20,),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 24.0),
-              child: Text('You may also like', style: Styles.textStyle18),
-            ),
-          ),
-          YouMayLikeList(),
         ],
       ),
     );
