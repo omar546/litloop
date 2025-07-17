8f953151ea73672e5e963468462290f5fa86df45
omar546 <omer.mkawy.om@gmail.com>
2025-07-17 12:23:37 +0300

    add either class

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,6 +1,9 @@
+import 'package:dartz/dartz.dart';
 import 'package:litloop/features/home/domain/entities/book_entity.dart';
 
+import '../../../../core/errors/failure.dart';
+
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
