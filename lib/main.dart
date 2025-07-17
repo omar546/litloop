115ae1a9ed5549c61b7686beaa3f24ee4f450f15
omar546 <omer.mkawy.om@gmail.com>
2025-07-17 14:11:29 +0300

    Integrate Hive and register BookEntityAdapter
    This commit introduces Hive for local data storage and registers the `BookEntityAdapter`.
    
    - Added `kFeaturedBox` constant.
    - Initialized Hive and opened the `featuredBox` in `main.dart`.
    - Registered `BookEntityAdapter` with Hive.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:litloop/core/utils/app_router.dart';
 import 'constants.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+import 'features/home/domain/entities/book_entity.dart';
+
+void main() async {
   runApp(const LitLoopApp());
+
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
 }
 
 class LitLoopApp extends StatelessWidget {
