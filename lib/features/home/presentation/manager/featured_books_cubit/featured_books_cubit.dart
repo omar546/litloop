c6ddea3c131212a177303fbcb1e696e1d394bc08
omar546 <omer.mkawy.om@gmail.com>
2025-07-18 17:48:28 +0300

    featured_books_cubit.dart

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:litloop/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:meta/meta.dart';
 
 import '../../../domain/entities/book_entity.dart';
@@ -6,5 +7,19 @@ import '../../../domain/entities/book_entity.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.featuredBooksUseCase) : super(FeaturedBooksInitial());
+  final FetchFeaturedBooksUseCase featuredBooksUseCase;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    try {
+      var result = await featuredBooksUseCase.call();
+      result.fold(
+        (failure) => emit(FeaturedBooksFailure(failure.message)),
+        (books) => emit(FeaturedBooksSuccess(books)),
+      );
+    } catch (e) {
+      emit(FeaturedBooksFailure(e.toString()));
+    }
+  }
 }
